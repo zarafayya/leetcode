f253eb377ff4297fc243686c496ac9e4abe4e094
zarafayya <zahra.fayyadiyati@gmail.com>
2025-07-02 02:34:58 +0700

    add: problem 3

---
+++ Problems/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,22 @@
+#include <map>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int result = 0;
+        int start = 0;
+        int size = s.size();
+        map<char, int> table;
+
+        for (int end=0;end<size;end++) {
+            // this is the devil. count() method is used to check whether a key exist in c++ map.
+            if (table.count(s[end]) && table[s[end]] >= start) {
+                start = table[s[end]] + 1;
+            }
+            result = max(result, end - start + 1);
+            table[s[end]] = end;
+        }
+
+        return result;
+    }
+};
